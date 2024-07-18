1dd920c0d3cd1694d07c12c90428287b43e91e55
mauro-arcelles <mauroar1001@hotmail.com>
2024-07-17 19:39:32 -0500

    first commit

---
+++ app/todos/page.tsx
@@ -0,0 +1,22 @@
+import { TodoList } from "@/components/todo-list";
+import { Separator } from "@/components/ui/separator";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function TodosPage() {
+  const supabase = createClient();
+
+  const { data: todos } = await supabase
+    .from("todos")
+    .select()
+    .order("inserted_at", { ascending: false });
+
+  return (
+    <section className="p-3 pt-6 max-w-2xl w-full flex flex-col gap-4">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Todo's
+      </h1>
+      <Separator className="w-full " />
+      <TodoList todos={todos ?? []} />
+    </section>
+  );
+}
